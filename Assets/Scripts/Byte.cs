4b5fddb71407eeb3275903362cf52a9098a05adf
Adem-ma <37601@ma-web.nl>
2025-04-12 17:43:30 +0200

    committt

--- Assets/Scripts/Byte.cs
+++ Assets/Scripts/Byte.cs
@@ -1,31 +1,93 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Byte : MonoBehaviour
 {
-    [SerializeField] BitInt[] bits = new BitInt[0];
-    [SerializeField] int value = 0;
+    [SerializeField] BitInt[] bits = new BitInt[8];
+    [SerializeField] private int value = 0;
+
     void Update()
     {
-
         Bin2Dec();
-
-        Console.WriteLine(value);
-
+        Debug.Log(value);
     }
+
     private void Bin2Dec()
     {
-        value = 0;
-
-        for (int i = 0; i < bits.Length; i++)
+        this.value = 0;
+        if (bits[0].state) { this.value += 1; }
+        if (bits[1].state) { this.value += 2; }
+        if (bits[2].state) { this.value += 4; }
+        if (bits[3].state) { this.value += 8; }
+        if (bits[4].state) { this.value += 16; }
+        if (bits[5].state) { this.value += 32; }
+        if (bits[6].state) { this.value += 64; }
+        if (bits[7].state) { this.value += 128; }
+    }
+    public void SetValue(int newValue)
+    {
+        if (newValue > 255)
         {
-            if (bits[i].state)
-            {
-                value += (1 << i);
-            }
+            newValue = 255;
         }
-            Debug.Log("W");
+        this.value = newValue;
+        if (newValue >= 128)
+        {
+            bits[7].state = true;
+            newValue -= 128;
+        }
+        else
+        {
+            bits[7].state = false;
+        }
+        if (newValue >= 64)
+        {
+            bits[6].state = true;
+            newValue -= 64;
+        }
+        else { bits[6].state = false; }
+        if (newValue >= 32)
+        {
+            bits[5].state = true;
+            newValue -= 32;
+        }
+        else { bits[5].state = false; }
+        if (newValue >= 16)
+        {
+            bits[4].state = true;
+            newValue -= 16;
+        }
+        else { bits[4].state = false; }
+        if (newValue >= 8)
+        {
+            bits[3].state = true;
+            newValue -= 8;
+        }
+        else { bits[3].state = false; }
+        if (newValue >= 4)
+        {
+            bits[2].state = true;
+            newValue -= 4;
+        }
+        else { bits[2].state = false; }
+        if (newValue >= 2)
+        {
+            bits[1].state = true;
+            newValue -= 2;
+        }
+        else { bits[1].state = false; }
+        if (newValue >= 1)
+        {
+            bits[0].state = true;
+            newValue -= 1;
+        }
+        else { bits[0].state = false; }
+
+    }
+    public string getHex()
+    {
+        string HexValue = value.ToString("X2");
+        return HexValue;
     }
 }
